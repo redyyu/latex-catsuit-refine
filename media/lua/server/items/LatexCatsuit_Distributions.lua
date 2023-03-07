b927ed06ed86bf79644090c20fd9bc9aa9e154eb
Redy <redy.ru@gmail.com>
2023-03-07 16:39:50 +0800

    ready

---
+++ media/lua/server/items/LatexCatsuit_Distributions.lua
@@ -0,0 +1,88 @@
+--BedroomDresser
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Gloves");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Stockings");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Pants");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Pants_Open");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Skirt");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Bolero");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Leotard");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Hood");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Catsuit");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.2);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Latex_Catsuit_Nude");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.1);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Cuffs_Collar");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.2);
+
+table.insert(ProceduralDistributions.list.BedroomDresser.items, "Leather_Harness");
+table.insert(ProceduralDistributions.list.BedroomDresser.items, 0.2);
+
+
+--WardrobeWoman
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Gloves");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Stockings");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Pants");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Pants_Open");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Skirt");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Bolero");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Leotard");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Hood");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Catsuit");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Latex_Catsuit_Nude");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.1);
+
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Cuffs_Collar");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, "Leather_Harness");
+table.insert(ProceduralDistributions.list.WardrobeWoman.items, 0.2);
+
+--StripClubDressers
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Gloves");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Stockings");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Pants");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Pants_Open");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Skirt");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Bolero");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Leotard");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Hood");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Catsuit");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 20);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Latex_Catsuit_Nude");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 10);
+table.insert(ProceduralDistributions.list.StripClubDressers.items, "Leather_Harness");
+table.insert(ProceduralDistributions.list.StripClubDressers.items, 20);
---
+++ media/lua/shared/NPCs/LatexCatsuitZombiesZoneDefinition.lua
@@ -0,0 +1,9 @@
+table.insert(ZombiesZoneDefinition.Default, {name = "LatexCatsuitGirl", chance=100.2});
+
+table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirl", chance=1});
+
+table.insert(ZombiesZoneDefinition.Rich, {name = "LatexCatsuitGirl", chance=2});
+table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirl", chance=2});
+table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirl", chance=2});
+
+table.insert(ZombiesZoneDefinition.CountryClub, {name = "LatexCatsuitGirl", chance=5});
