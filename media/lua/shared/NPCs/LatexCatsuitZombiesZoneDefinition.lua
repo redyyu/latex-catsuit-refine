ad0640a247736fc3e542fbb24efd179a440b552a
redyyu <redy.ru@gmail.com>
2024-01-30 18:39:01 +0800

    change rate

--- media/lua/shared/NPCs/LatexCatsuitZombiesZoneDefinition.lua
+++ media/lua/shared/NPCs/LatexCatsuitZombiesZoneDefinition.lua
@@ -3,25 +3,25 @@ table.insert(ZombiesZoneDefinition.Default, {name = "LatexCatsuitGirlBlack", cha
 table.insert(ZombiesZoneDefinition.Default, {name = "LatexCatsuitGirlRed", chance=0.2});
 table.insert(ZombiesZoneDefinition.Default, {name = "LatexCatsuitGirlFull", chance=0.2});
 
-table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlNude", chance=0.1});
-table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlBlack", chance=0.1});
-table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlRed", chance=0.1});
-table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlFull", chance=0.1});
+table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlNude", chance=0.05});
+table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlBlack", chance=0.05});
+table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlRed", chance=0.05});
+table.insert(ZombiesZoneDefinition.Bar, {name = "LatexCatsuitGirlFull", chance=0.05});
 
 table.insert(ZombiesZoneDefinition.Rich, {name = "LatexCatsuitGirlNude", chance=0.2});
 table.insert(ZombiesZoneDefinition.Rich, {name = "LatexCatsuitGirlBlack", chance=0.2});
 table.insert(ZombiesZoneDefinition.Rich, {name = "LatexCatsuitGirlRed", chance=0.2});
 table.insert(ZombiesZoneDefinition.Rich, {name = "LatexCatsuitGirlFull", chance=0.2});
 
-table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlNude", chance=0.5});
-table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlBlack", chance=0.5});
-table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlRed", chance=0.5});
-table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlFull", chance=0.5});
+table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlNude", chance=0.2});
+table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlBlack", chance=0.2});
+table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlRed", chance=0.2});
+table.insert(ZombiesZoneDefinition.HotelRich, {name = "LatexCatsuitGirlFull", chance=0.2});
 
-table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlNude", chance=0.5});
-table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlBlack", chance=0.5});
-table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlRed", chance=0.5});
-table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlFull", chance=0.5});
+table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlNude", chance=0.2});
+table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlBlack", chance=0.2});
+table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlRed", chance=0.2});
+table.insert(ZombiesZoneDefinition.FancyHotel, {name = "LatexCatsuitGirlFull", chance=0.2});
 
 table.insert(ZombiesZoneDefinition.CountryClub, {name = "LatexCatsuitGirlNude", chance=0.2});
 table.insert(ZombiesZoneDefinition.CountryClub, {name = "LatexCatsuitGirlBlack", chance=0.2});
