b01e11cb540a01950faf0a16ee71c879a7d59783
redyyu <redy.ru@gmail.com>
2024-02-14 21:56:03 +0800

    change latex suits to bag, only spawn bags on room.

--- media/lua/server/items/LatexCatsuit_Distributions.lua
+++ media/lua/server/items/LatexCatsuit_Distributions.lua
@@ -1,57 +1,77 @@
 require "Items/ProceduralDistributions"
+require "Items/SuburbsDistributions"
 
 MOD_NAME = "LatexCatsuitRefine";
 
-local ITEMS_WEIGHT = {
-    [".LatexCatsuitMag"]=0.05,
-    [".LatexCatsuitFishnet"]=0.1,
-    [".LatexCatsuitTightsBlack"]=0.1,
-    [".LatexCatsuitTightsBlackSemiTrans"]=0.1,
-    [".LatexCatsuitTightsBlackTrans"]=0.1,
-    [".LatexCatsuitNude"]=0.1,
-    [".LatexCatsuitBlack"]=0.1,
-    [".LatexCatsuitRed"]=0.1,
-    [".LatexHoodNude"]=0.1,
-    [".LatexHoodBlack"]=0.1,
-    [".LatexHoodRed"]=0.1,
-    [".LatexLeotardNude"]=0.1,
-    [".LatexLeotardBlack"]=0.1,
-    [".LatexLeotardRed"]=0.1,
-    [".LatexLeotardHNude"]=0.1,
-    [".LatexLeotardHBlack"]=0.1,
-    [".LatexLeotardHRed"]=0.1,
-    [".LatexLeotardSNude"]=0.1,
-    [".LatexLeotardSBlack"]=0.1,
-    [".LatexLeotardSRed"]=0.1,
-    [".LatexBoleroNude"]=0.1,
-    [".LatexBoleroBlack"]=0.1,
-    [".LatexBoleroRed"]=0.1,
-    [".LatexPantsNude"]=0.1,
-    [".LatexPantsBlack"]=0.1,
-    [".LatexPantsRed"]=0.1,
-    [".LatexPantsOpenNude"]=0.1,
-    [".LatexPantsOpenBlack"]=0.1,
-    [".LatexPantsOpenRed"]=0.1,
-    [".LatexSkirtBlack"]=0.1,
-    [".LatexSkirtRed"]=0.1,
-    [".LatexStockingsNude"]=0.1,
-    [".LatexStockingsBlack"]=0.1,
-    [".LatexStockingsRed"]=0.1,
-    [".LatexStockingsHNude"]=0.1,
-    [".LatexStockingsHBlack"]=0.1,
-    [".LatexStockingsHRed"]=0.1,
-    [".LatexGlovesNude"]=0.1,
-    [".LatexGlovesBlack"]=0.1,
-    [".LatexGlovesRed"]=0.1,
-    [".HarnessRope"]=0.1,
-    [".HarnessBlack"]=0.1,
-    [".HarnessRed"]=0.1,
-    [".HarnessWRope"]=0.1,
-    [".HarnessWBlack"]=0.1,
-    [".HarnessWRed"]=0.1,
-    [".CuffsCollar"]=0.1,
-    [".Bag_LatexBag"]=0.1,
-    [".Bag_LatexBagRed"]=0.1,
+local SUB_BLACK_ITEMS = {
+    [".LatexCatsuitMag"]=0.5,
+    [".LatexCatsuitFishnet"]=15,
+    [".LatexCatsuitTightsBlack"]=15,
+    [".LatexCatsuitTightsBlackSemiTrans"]=15,
+    [".LatexCatsuitTightsBlackTrans"]=15,
+    [".LatexCatsuitNude"]=15,
+    [".LatexCatsuitBlack"]=30,
+    [".LatexHoodNude"]=5,
+    [".LatexHoodBlack"]=10,
+    [".LatexLeotardNude"]=5,
+    [".LatexLeotardBlack"]=10,
+    [".LatexLeotardHNude"]=5,
+    [".LatexLeotardHBlack"]=10,
+    [".LatexLeotardSNude"]=5,
+    [".LatexLeotardSBlack"]=10,
+    [".LatexBoleroNude"]=5,
+    [".LatexBoleroBlack"]=10,
+    [".LatexPantsNude"]=5,
+    [".LatexPantsBlack"]=10,
+    [".LatexPantsOpenNude"]=5,
+    [".LatexPantsOpenBlack"]=10,
+    [".LatexSkirtBlack"]=15,
+    [".LatexStockingsNude"]=5,
+    [".LatexStockingsBlack"]=10,
+    [".LatexStockingsHNude"]=5,
+    [".LatexStockingsHBlack"]=10,
+    [".LatexGlovesNude"]=5,
+    [".LatexGlovesBlack"]=10,
+    [".HarnessRope"]=5,
+    [".HarnessBlack"]=5,
+    [".HarnessWRope"]=5,
+    [".HarnessWBlack"]=5,
+    [".CuffsCollar"]=10,
+}
+
+local SUB_RED_ITEMS = {
+    [".LatexCatsuitMag"]=0.5,
+    [".LatexCatsuitFishnet"]=15,
+    [".LatexCatsuitTightsBlackSemiTrans"]=15,
+    [".LatexCatsuitTightsBlackTrans"]=15,
+    [".LatexCatsuitNude"]=15,
+    [".LatexCatsuitRed"]=30,
+    [".LatexHoodNude"]=5,
+    [".LatexHoodRed"]=10,
+    [".LatexLeotardNude"]=5,
+    [".LatexLeotardRed"]=10,
+    [".LatexLeotardHNude"]=5,
+    [".LatexLeotardHRed"]=10,
+    [".LatexLeotardSNude"]=5,
+    [".LatexLeotardSRed"]=10,
+    [".LatexBoleroNude"]=5,
+    [".LatexBoleroRed"]=10,
+    [".LatexPantsNude"]=5,
+    [".LatexPantsRed"]=10,
+    [".LatexPantsOpenNude"]=5,
+    [".LatexPantsOpenRed"]=10,
+    [".LatexSkirtRed"]=15,
+    [".LatexStockingsNude"]=5,
+    [".LatexStockingsRed"]=10,
+    [".LatexStockingsHNude"]=5,
+    [".LatexStockingsHRed"]=10,
+    [".LatexGlovesNude"]=5,
+    [".LatexGlovesRed"]=10,
+    [".HarnessRope"]=5,
+    [".HarnessRed"]=5,
+    [".HarnessWRope"]=5,
+    [".HarnessWRed"]=5,
+    [".CuffsCollar"]=10,
 }
 
 
@@ -70,19 +90,57 @@ local function insertTable(table_obj, key, weight)
     end
 end 
 
-local function insertDistributionList(table_obj, ITEMS_WEIGHT, rate)
-    for k, v in pairs(ITEMS_WEIGHT) do
-        insertTable(table_obj, k, v*rate);
-    end
-end
-
-
-insertDistributionList(ProceduralDistributions.list.BedroomDresser, ITEMS_WEIGHT, 0.01);
-insertDistributionList(ProceduralDistributions.list.WardrobeWoman, ITEMS_WEIGHT, 0.01);
-insertDistributionList(ProceduralDistributions.list.StripClubDressers, ITEMS_WEIGHT, 6);
 
 insertTable(ProceduralDistributions.list["CrateBooks"], ".LatexCatsuitMag", 0.01);
 insertTable(ProceduralDistributions.list["BookstoreMisc"], ".LatexCatsuitMag", 0.01);
 insertTable(ProceduralDistributions.list["CrateMagazines"], ".LatexCatsuitMag", 0.01);
--- insertTable(ProceduralDistributions.list["LibraryBooks"], ".LatexCatsuitMag", 0.01);
+insertTable(ProceduralDistributions.list["LibraryBooks"], ".LatexCatsuitMag", 0.01);
 insertTable(ProceduralDistributions.list["MagazineRackMixed"], ".LatexCatsuitMag", 0.01);
+
+insertTable(ProceduralDistributions.list.BedroomDresser, ".LatexCatsuitMag", 0.01);
+insertTable(ProceduralDistributions.list.WardrobeWoman, ".LatexCatsuitMag", 0.01);
+insertTable(ProceduralDistributions.list.StripClubDressers, ".LatexCatsuitMag", 0.01);
+
+
+local function insertSuburbsItems(key, items)
+    SuburbsDistributions[key] = {
+        rolls = 4,
+        items = {},
+        junk = {
+            rolls = 1,
+            items = {
+                
+            }
+        },
+        fillRand = 0,
+    }
+
+    for k, v in pairs(items) do
+        if k:find(".", 1, true) == 1 then
+            k = MOD_NAME..k
+        end
+        table.insert(SuburbsDistributions[key].items, k);
+	    table.insert(SuburbsDistributions[key].items, v);
+    end
+    
+end
+
+-- !!! DO NOT add package name before items when injuect SuburbsDistributions table.
+insertSuburbsItems('Bag_LatexBagBlack', SUB_BLACK_ITEMS)
+insertSuburbsItems('Bag_LatexBagRed', SUB_RED_ITEMS)
+
+
+insertTable(ProceduralDistributions.list.BedroomDresser, ".Bag_LatexBagBlack", 500.01);
+insertTable(ProceduralDistributions.list.BedroomDresser, ".Bag_LatexBagRed", 0.01);
+
+insertTable(ProceduralDistributions.list.WardrobeWoman, ".Bag_LatexBagBlack", 500.01);
+insertTable(ProceduralDistributions.list.WardrobeWoman, ".Bag_LatexBagRed", 0.01);
+
+insertTable(ProceduralDistributions.list.StripClubDressers, ".Bag_LatexBagBlack", 500.01);
+insertTable(ProceduralDistributions.list.StripClubDressers, ".Bag_LatexBagRed", 0.01);
+
+insertTable(ProceduralDistributions.list.KitchenPots, ".Bag_LatexBagBlack", 500.01);
+insertTable(ProceduralDistributions.list.KitchenPots, ".Bag_LatexBagRed", 0.01);
+
+insertTable(ProceduralDistributions.list.StoreKitchenPots, ".Bag_LatexBagBlack", 500.01);
+insertTable(ProceduralDistributions.list.StoreKitchenPots, ".Bag_LatexBagRed", 0.01);
