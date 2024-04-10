bb4da47e01629d113d33899f2432196923eee75b
redyyu <redy.ru@gmail.com>
2024-04-10 19:20:54 +0800

    xxx

--- media/lua/shared/NPCs/LatexCatsuitZombiesZoneDefinition.lua
+++ media/lua/shared/NPCs/LatexCatsuitZombiesZoneDefinition.lua
@@ -28,3 +28,123 @@ table.insert(ZombiesZoneDefinition.CountryClub, {name = "LatexCatsuitGirlBlack",
 table.insert(ZombiesZoneDefinition.CountryClub, {name = "LatexCatsuitGirlRed", chance=0.5})
 table.insert(ZombiesZoneDefinition.CountryClub, {name = "LatexCatsuitGirlFull", chance=0.5})
 
+
+if ZombiesZoneDefinition.Stripclub then
+    table.insert(ZombiesZoneDefinition.Stripclub, {name = "LatexCatsuitGirlNude", chance=75})
+    table.insert(ZombiesZoneDefinition.Stripclub, {name = "LatexCatsuitGirlBlack", chance=75})
+    table.insert(ZombiesZoneDefinition.Stripclub, {name = "LatexCatsuitGirlRed", chance=75})
+    table.insert(ZombiesZoneDefinition.Stripclub, {name = "LatexCatsuitGirlFull", chance=75})
+else
+    ZombiesZoneDefinition.Stripclub = {
+        chanceToSpawn = 100,
+        StripperBlackM = {
+            name="StripperBlack",
+            toSpawn=2,
+            gender="female",
+            mandatory="true",
+        },
+        StripperNakedM = {
+            name="StripperNaked",
+            toSpawn=1,
+            gender="female",
+            mandatory="true",
+        },
+        StripperPinkM = {
+            name="StripperPink",
+            toSpawn=2,
+            gender="female",
+            mandatory="true",
+        },
+        StripperBlack = {
+            name="StripperBlack",
+            gender="female",
+            chance=75,
+        },
+        StripperNaked = {
+            name="StripperNaked",
+            gender="female",
+            chance=75,
+        },
+        StripperPink = {
+            name="StripperPink",
+            gender="female",
+            chance=75,
+        },
+        WaiterStripper = {
+            name="WaiterStripper",
+            gender="female",
+            chance=75,
+        },
+        LatexCatsuitGirlNude = {
+            name="LatexCatsuitGirlNude",
+            gender="female",
+            chance=75,
+        },
+        LatexCatsuitGirlBlack = {
+            name="LatexCatsuitGirlBlack",
+            gender="female",
+            chance=75,
+        },
+        LatexCatsuitGirlRed = {
+            name="LatexCatsuitGirlRed",
+            gender="female",
+            chance=75,
+        },
+        LatexCatsuitGirlFull = {
+            name="LatexCatsuitGirlFull",
+            gender="female",
+            chance=75,
+        },
+        Classy = {
+            name="Classy",
+            gender="male",
+            chance=75,
+        },
+    }
+end
+
+if ZombiesZoneDefinition.Nightclub then
+    table.insert(ZombiesZoneDefinition.Nightclub, {name = "LatexCatsuitGirlNude", chance=50})
+    table.insert(ZombiesZoneDefinition.Nightclub, {name = "LatexCatsuitGirlBlack", chance=50})
+    table.insert(ZombiesZoneDefinition.Nightclub, {name = "LatexCatsuitGirlRed", chance=50})
+    table.insert(ZombiesZoneDefinition.Nightclub, {name = "LatexCatsuitGirlFull", chance=50})
+else
+    ZombiesZoneDefinition.Nightclub = {
+        chanceToSpawn = 100,
+        HookerM = {
+            name="Hooker",
+            toSpawn=3,
+            gender="female",
+            mandatory="true",
+        },
+        Hooker = {
+            name="Hooker",
+            gender="female",
+            chance=75,
+        },
+        LatexCatsuitGirlNude = {
+            name="LatexCatsuitGirlNude",
+            gender="female",
+            chance=50,
+        },
+        LatexCatsuitGirlBlack = {
+            name="LatexCatsuitGirlBlack",
+            gender="female",
+            chance=50,
+        },
+        LatexCatsuitGirlRed = {
+            name="LatexCatsuitGirlRed",
+            gender="female",
+            chance=50,
+        },
+        LatexCatsuitGirlFull = {
+            name="LatexCatsuitGirlFull",
+            gender="female",
+            chance=50,
+        },
+        Classy = {
+            name="Classy",
+            chance=75,
+        },
+    }
+end
